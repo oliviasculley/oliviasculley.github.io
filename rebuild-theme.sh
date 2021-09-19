f915d7d35cf6fe3bfbaf3c8d4c64c05bdd86b060
oliviasculley <olivia@sculley.dev>
2021-09-19 15:53:34 -0400

    add gitignore, asdf and tweak script

--- rebuild-theme.sh
+++ rebuild-theme.sh
@@ -1,4 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -eoux pipefail
 
-(cd themes/tranquilpeak && npm run build) && hugo server
+(cd themes/tranquilpeak && npm run build) && \
+  hugo server \
+    --port=4000 \
+    --bind=0.0.0.0 \
+    --baseURL=https://dev.sculley.dev \
+    --appendPort=false
